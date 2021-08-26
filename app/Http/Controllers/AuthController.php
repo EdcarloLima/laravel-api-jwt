9cf2e1d6873a6bdd0f887808e83198207a249dc0
Edcarlo Lima <edcarlolimsan@gmail.com>
2021-08-25 22:38:49 -0300

    Criando o controller, request e rota de login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $input = $request->validated();
+        dd($input);
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+}
