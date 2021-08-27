2ace2466ba974aeaffb0284c63775c6e9d6e7576
Edcarlo Lima <edcarlolimsan@gmail.com>
2021-08-26 22:14:28 -0300

    migrate, seed e login response json token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,13 +10,20 @@ class AuthController extends Controller
     public function login(LoginRequest $request)
     {
         $input = $request->validated();
-        dd($input);
-        $credentials = request(['email', 'password']);
+
+        $credentials = [
+            'email'    => $input['email'],
+            'password' => $input['password']
+        ];
 
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
 }
